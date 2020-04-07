1dcc09b780d74726a8de42491467ce074a8bb0cb
Hubert Wong <hubertwwong@gmail.com>
2020-04-07 10:43:09 -0700

    Adding pipenv

--- install/python.sh
+++ install/python.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "> install > python 3"
 sudo apt -y install python3 python3-dev python3-venv
-echo "> install > python 3 pip. This is the source version."
+echo "> install > pipenv. Note you should probably use this over virtual env."
+pip3 install --user pipenv
+# echo "> install > python 3 pip. This is the source version."
 # Weirdness of intsalling int using python2.7
 #wget https://bootstrap.pypa.io/get-pip.py
 #sudo python get-pip.py
