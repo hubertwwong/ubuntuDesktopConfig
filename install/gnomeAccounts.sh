cfdf1923dec0faac4ad3b1a35e4e1af420600cc1
Hubert Wong <hubertwwong@gmail.com>
2020-04-08 13:21:14 -0700

    Added docs

--- install/gnomeAccounts.sh
+++ install/gnomeAccounts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "> gnome online accounts > used for online accounts"
 sudo apt install -y gnome-control-center gnome-online-accounts
+# Note. Needed for added google and ubuntu sso to file explorer
+# Only needed if you want to want to add these features to a non ubuntu os. 
