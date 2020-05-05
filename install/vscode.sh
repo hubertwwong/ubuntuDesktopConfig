3fa7798c3ee44bd22ac1dbb25997e0439ec8cc10
Hubert Wong <hubertwwong@gmail.com>
2020-05-05 12:03:23 -0700

    Adding watch limit size increase to vscode

--- install/vscode.sh
+++ install/vscode.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "> Install > VSCode"
 sudo snap install --classic code
+echo "> Update filewatch limit"
+echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
