d037402c2ca2858a6afeceeb7d5b2a6d0d17eda5
Hubert Wong <hubertwwong@gmail.com>
2020-05-05 12:39:06 -0700

    Added some more package to remove

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "> uninstall > removing some uneeded packages"
+sudo apt update
 sudo apt -y remove --purge libreoffice*
+#https://wiki.ubuntu.com/LibreOffice
+sudo apt -y remove --purge libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw libreoffice-base libreoffice-math libreoffice-help-* libreoffice-l10n-* libreoffice-nlpsolver libreoffice-l10n-fr myspell-fr hyphen-fr mythes-fr libreoffice-help-fr 
 sudo apt -y remove --purge thunderbird
 sudo apt -y remove --purge gimp
 sudo apt -y autoremove
